9d5f41aadb677c78a877d6c90baa88f3ddc0f866
Sergio Bastos <giftc444@gmail.com>
2025-09-25 21:44:42 -0300

    A tela de login foi implementada

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello Wolrd</h1>
-    </>
+    <Login />
   )
 }
 
