45213d9bf924d273502368b95b6b90d970727462
Sergio Bastos <giftc444@gmail.com>
2025-09-26 15:40:53 -0300

    Verificação de login e registro

--- website/src/components/Cadastro.jsx
+++ website/src/components/Cadastro.jsx
@@ -3,9 +3,40 @@ import { useNavigate } from "react-router-dom";
 import Input from "./Input";
 import Label from "./Label";
 import Button from "./Button";
+import { useState } from "react";
 
 function Cadastro() {
   const navigate = useNavigate();
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  function verificarRegistro(e) {
+    e.preventDefault();
+    if (
+      !user.trim() ||
+      !password.trim() ||
+      !confirmPassword.trim() ||
+      !email.trim()
+    ) {
+      return alert("Preencha todas as informações.");
+    }
+    console.log(
+      "User:",
+      user,
+      "Password:",
+      password,
+      "ConfirmPassword:",
+      confirmPassword,
+      "Email:",
+      email
+    );
+    setUser("");
+    setPassword("");
+    setConfirmPassword("");
+    setEmail("");
+  }
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
@@ -19,22 +50,39 @@ function Cadastro() {
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
           Cadastro
         </h2>
-        <form className="space-y-4">
+        <form onSubmit={verificarRegistro} className="space-y-4">
           <div>
             <Label className="block text-gray-600 text-sm mb-1">Usuário</Label>
-            <Input type="text" placeholder="Digite seu usuário" />
+            <Input
+              type="text"
+              placeholder="Digite seu usuário"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
           </div>
           <div>
             <Label>Email</Label>
-            <Input type="email" placeholder="Insira o seu email"></Input>
+            <Input
+              type="email"
+              placeholder="Insira o seu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Input>
           </div>
           <div>
             <Label className="block text-gray-600 text-sm mb-1">Senha</Label>
-            <Input type="password" placeholder="Digite sua senha" />
+            <Input
+              type="password"
+              placeholder="Digite sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <input
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-3"
               type="password"
               placeholder="Confirme sua senha"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
           <div className="flex flex-col gap-2">
--- website/src/components/Login.jsx
+++ website/src/components/Login.jsx
@@ -2,25 +2,41 @@ import { useNavigate } from "react-router-dom";
 import Input from "./Input";
 import Label from "./Label";
 import Button from "./Button";
+import { useState } from "react";
 
 function Login() {
   const navigate = useNavigate();
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
   function vaiParaCadastro() {
     navigate(`/cadastro`);
   }
+
+  function verificarLogin(e) {
+    e.preventDefault();
+    if (!user.trim() || !password.trim()) {
+      return alert("Preencha o seu usuário e sua senha.");
+    }
+    console.log("User:", user, "Password:", password);
+    setUser("");
+    setPassword("");
+  }
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="w-full max-w-sm bg-white p-8 rounded-2xl shadow-lg">
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
           Login
         </h2>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={verificarLogin}>
           <div>
             <Label className="block text-gray-600 text-sm mb-1">Usuário</Label>
             <Input
               type="text"
               placeholder="Digite seu usuário"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
           </div>
@@ -29,12 +45,14 @@ function Login() {
             <Input
               type="password"
               placeholder="Digite sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
           </div>
           <div className="flex flex-col gap-2">
             <Button type="submit">Entre</Button>
-            <Button type="button" onClick={() => vaiParaCadastro()}>
+            <Button type="button" onClick={vaiParaCadastro}>
               Cadastre-se
             </Button>
           </div>
