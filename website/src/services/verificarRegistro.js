595a0644b0de43c37baae33d05731258b87cecfe
Sergio Bastos <giftc444@gmail.com>
2025-10-02 19:29:44 -0300

    Interação do front com a API

--- website/src/services/verificarRegistro.js
+++ website/src/services/verificarRegistro.js
@@ -1,46 +1,17 @@
 import User from "./user";
-import jwtEncode from "jwt-encode";
 
-function verificarRegistro({
-  name,
-  email,
-  password,
-  confirmPassword,
-  navigate,
-}) {
-  console.log("Dados recebidos no verificarRegistro:", {
-    name,
-    email,
-    password,
-    confirmPassword,
-  });
-
+function verificarRegistro({ name, email, password, confirmPassword }) {
   const novoUser = new User(name, email, password);
 
   if (!novoUser.validarCampos()) {
-    alert("Preencha todas as informações.");
-    return;
+    return { valido: false, mensagem: "Preencha todas as informações." };
   }
 
   if (password !== confirmPassword) {
-    alert("A senha não coincide. Mude ou tente novamente");
-    return;
+    return { valido: false, mensagem: "A senha não coincide." };
   }
 
-  const secret = import.meta.env.VITE_JWT_SECRET;
-  const token = jwtEncode(
-    {
-      name: novoUser.name,
-      email: novoUser.email,
-      password: novoUser.password, // só para teste local
-    },
-    secret
-  );
-
-  localStorage.setItem("user", token);
-
-  alert("Usuário cadastrado com sucesso!");
-  navigate("/");
+  return { valido: true, user: novoUser };
 }
 
 export default verificarRegistro;
