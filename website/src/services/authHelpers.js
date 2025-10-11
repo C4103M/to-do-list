4a542b07d73fec39c6f6c47a6358f1494cd049ce
Sergio Bastos <giftc444@gmail.com>
2025-10-10 22:26:01 -0300

    Alterações 2.0

--- website/src/services/authHelpers.js
+++ website/src/services/authHelpers.js
@@ -1,46 +1,41 @@
 import { getUserFromStorage } from "./auth";
 
-export function verificarLogin(userInput, passwordInput) {
-  if (!userInput.trim() || !passwordInput.trim()) {
+export function verificarLogin(emailInput, passwordInput) {
+  if (!emailInput.trim() || !passwordInput.trim()) {
     return false;
   }
 
   const savedUser = getUserFromStorage();
   if (!savedUser) return false;
 
-  if (savedUser.name === userInput && savedUser.password === passwordInput) {
+  if (savedUser.name === emailInput && savedUser.password === passwordInput) {
     return true;
   }
 
   return false;
 }
 
-export async function vaiParaHome(e, user, password, navigate, resetInputs) {
+export async function vaiParaHome(e, form_data, navigate, resetInputs) {
   e.preventDefault();
-
-  if (!user.trim() || !password.trim()) {
-    alert("Preencha usuário e senha.");
-    return;
-  }
 
   try {
     const resposta = await fetch("http://localhost:8080/api/public/login", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: user, password }),
+      body: form_data,
     });
-
-    if (!resposta.ok) {
-      throw new Error("Usuário ou senha incorretos");
-    }
 
     const dados = await resposta.json();
 
-    localStorage.setItem("user", JSON.stringify(dados.user));
+    if(dados.status != 200){
+      throw new Error(dados.message)
+    }
+    else {
+      localStorage.setItem("token", dados.token);
+      alert("Login realizado com sucesso!");
+      resetInputs();
+      navigate("/home");
+    }
 
-    alert("Login realizado com sucesso!");
-    resetInputs();
-    navigate("/home");
   } catch (erro) {
     console.error(erro);
     alert("Falha ao realizar login.");
